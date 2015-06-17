e19476741db7aa51167529342795a12365666ae0
Benjamin James <benjamin-james@utulsa.edu>
2015-06-17 13:41:40 -0500

    more porcelain-ish

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 autoreconf -i || exit 1
 cat << EOF
 
@@ -7,5 +7,15 @@ usually ${prefix}/etc, so if you want it to install in /etc,
 run ./configure with --sysconfdir=/etc/gitd or --sysconfdir=/etc
 
 Also, ./configure by default will use ${prefix} as /usr/local.
+So, gitd.conf would usually be placed in /usr/local/etc/gitd.conf,
+and the binary would be installed to /usr/local/bin/gitd
+
+          "      m        # 
+  mmmm  mmm    mm#mm   mmm# 
+ #" "#    #      #    #" "# 
+ #   #    #      #    #   # 
+ "#m"#  mm#mm    "mm  "#m## 
+  m  #                      
+   ""                       
 EOF
 
